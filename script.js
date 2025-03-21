502b68cee12e19b738f987b6ba954e69bbfe276e
ian ayiro <ian.ayiro@student.moringa.com>
2025-03-21 10:43:48 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function toggleNav() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active"); // Toggle the "active" class
+});
