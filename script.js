07d017440b825d06483ad1ee06daca1bd87a268c
ian ayiro <ian.ayiro@student.moringa.com>
2025-03-21 22:43:58 +0300

    added images to my assets/images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function toggleNav() {
+ function toggleNav() {
     const navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("active"); // Toggle the "active" class
-});
+};
